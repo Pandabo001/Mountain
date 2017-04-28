0b8f4ab9f2b8ddf987f3a7026179b53e46cc18d0
Pandabo001 <15621.smnr@gmail.com>
2017-04-29 01:52:49 +0700

    Add files via upload
    Input a .dat file into a program.

--- MapDataDrawer.java
+++ MapDataDrawer.java
@@ -1,10 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
 public class MapDataDrawer{
   
   public int[][] grid;
   
   public MapDataDrawer(String filename, int rows, int cols){
     
+    grid = new int[rows][cols];
     
+    Scanner sc;
+    try {
+      
+      sc = new Scanner(new File(filename));
+      for (int i = 0; i < rows; i++){
+        
+        for (int j = 0; j < cols; j++){
+          
+          if (sc.hasNextInt()){
+            
+            grid[i][j] = sc.nextInt(); 
+          }
+        }
+        
+      }
+      
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     
   }
   
@@ -14,14 +39,7 @@ public class MapDataDrawer{
     
   }
   
-  public int drawLowestElevPath(){
-    
-  }
   
-  public int indexOfLowestElevPath(){
-    
-    
-  }
   
   
 }
