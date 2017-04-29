d5a0a1093ecfa0f04ce246a3a0aed3f248bb212d
tintin3rrr <tintin_yingsakul@hotmail.com>
2017-04-29 22:09:54 +0700

    Update MapDataDrawer

--- MapDataDrawer.java
+++ MapDataDrawer.java
@@ -97,14 +97,89 @@ public class MapDataDrawer{
   
 
   public int drawLowestElevPath(Graphics g, int row){
+    int max = findMaxValue();
+    int totalChange = 0;
     
+    for (int j = 0; j < grid[0].length - 1; j++){
+      
+      g.fillRect(j, row, 1, 1);
+      int fwd = grid[row][j + 1];
+      int fwd_up = -1;
+      int fwd_down = -1;
+      
+      if (row != 0){
+        
+        fwd_up = grid[row - 1][j + 1];
+      }
+      
+      if (row != grid.length - 1){
+        
+        fwd_down = grid[row + 1][j + 1];
+      }
+      
+      int current_fwd_diff = Math.abs(grid[row][j] - fwd);
+      int current_fwdup_diff = max + 1;
+      int current_fwddown_diff = max + 1;
+      if (fwd_up > -1){
+        
+        current_fwdup_diff = Math.abs(grid[row][j] - fwd_up);
+      }
+      
+      if (fwd_down > -1){
+        
+        current_fwddown_diff = Math.abs(grid[row][j] - fwd_down);
+      }
+      
+      int least = current_fwd_diff;
+      
+      if (current_fwd_diff > current_fwdup_diff){
+        
+        if (current_fwdup_diff > current_fwddown_diff){
+          
+          least = current_fwddown_diff;
+          row++;
+        }else{
+          
+          least = current_fwdup_diff;
+          row--;
+        }
+      }else{
+        
+        if (current_fwd_diff > current_fwddown_diff){
+          
+          least = current_fwddown_diff;
+          row++;
+          
+        }else{
+          least = current_fwd_diff;
+        }
+      }
+      
+      totalChange += least;
+      
+    }
+    
+    return totalChange;
   }
   
 
   public int indexOfLowestElevPath(Graphics g){
+    int least = drawLowestElevPath(g, 0);
+    int index = 0;
     
+    for (int i = 1; i < grid.length; i++){
+      
+      int change = drawLowestElevPath(g, i);
+      if (change < least){
+        least = change;
+        index = i;
+      }
+    }
+    
+    return index;
     
   }
+  
   
   
 }
